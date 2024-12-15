e969ebf4c1a8d92d6221c62566ec5aea9e3444a9
Roeich <roeichsheikh@gmail.com>
2024-12-15 16:33:51 +0600

    working create order page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    /* ________________ start dashboard page ________________ */
     $(".orders-date").flatpickr({
         defaultDate:"today",
         dateFormat: "d-m-Y",
@@ -6,4 +7,19 @@ $(document).ready(function(){
             console.log(dt);
         }
     });
+    /* ________________ end dashboard page ________________ */
+    
+    /* ________________ start track order page ________________ */
+    $(".orders-date2").flatpickr({
+        dateFormat: "d-m-Y",
+        onChange:(dt)=>{
+            console.log(dt);
+        }
+    });
+    // select2 initialize
+    $('.select2:not(.select2_searchable)').select2({
+        minimumResultsForSearch: -1
+    });
+    $('.select2.select2_searchable').select2();
+    /* ________________ end track order page ________________ */
 })
